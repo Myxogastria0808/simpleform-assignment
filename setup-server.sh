513d0c7b2964ac46e85a65cfa861ac92f38bcf93
Myxogastria0808 <r.rstudio.c@gmail.com>
2023-10-29 02:55:18 +0900

    second commit

--- setup-server.sh
+++ setup-server.sh
@@ -22,4 +22,21 @@ sudo mysql_secure_installation
 sudo mysql -u root -p
 #password 123abc
 
+#もし、pythonのバージョンが古かったら、以下のコマンドで上げる
+sudo apt update
+sudo apt install build-essential libbz2-dev libdb-dev \
+  libreadline-dev libffi-dev libgdbm-dev liblzma-dev \
+  libncursesw5-dev libsqlite3-dev libssl-dev \
+  zlib1g-dev uuid-dev tk-dev
+wget https://www.python.org/ftp/python/3.11.6/Python-3.11.6.tar.xz
+tar xJf Python-3.11.6.tar.xz
+cd Python-3.11.6
+./configure
+make
+sudo make install
+#一度ログアウト
+logout
+#以下で確認
+python3 -V
+
 crontab -e
